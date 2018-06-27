04b4e960183e4ea07694ce1c26f3525cbc6aa7a8
HarshithaAvuthu <31199062+HarshithaAvuthu@users.noreply.github.com>
2018-06-27 11:19:42 +0530

    Update highestproduct.java

--- Week1/day1/highestproduct.java
+++ Week1/day1/highestproduct.java
@@ -1,6 +1,84 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 public class Solution {
-    public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length-1] * nums[nums.length-2] * nums[nums.length- 3]);
+
+    public static int highestProductOf3(int[] intArray) {
+
+            Arrays.sort(intArray);
+            int P1=(intArray[0] * intArray[1] * intArray[intArray.length - 1]);
+            int P2=(intArray[intArray.length-1] * intArray[intArray.length-2] * intArray[intArray.length- 3]);
+
+            // return Math.max(intArray[0] * intArray[1] * intArray[intArray.length - 1], intArray[intArray.length-1] * intArray[intArray.length-2] * intArray[intArray.length- 3]);
+            return Math.max(P1,P2);
+    }
+
+
+
+    // tests
+
+    @Test
+    public void shortArrayTest() {
+        final int actual = highestProductOf3(new int[] {1, 2, 3, 4});
+        final int expected = 24;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void longerArrayTest() {
+        final int actual = highestProductOf3(new int[] {6, 1, 3, 5, 7, 8, 2});
+        final int expected = 336;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayHasOneNegativeTest() {
+        final int actual = highestProductOf3(new int[] {-5, 4, 8, 2, 3});
+        final int expected = 96;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayHasTwoNegativesTest() {
+        final int actual = highestProductOf3(new int[] {-10, 1, 3, 2, -10});
+        final int expected = 300;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayIsAllNegativesTest() {
+        final int actual = highestProductOf3(new int[] {-5, -1, -3, -2});
+        final int expected = -6;
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyArrayTest() {
+        highestProductOf3(new int[] {});
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithOneNumberTest() {
+        highestProductOf3(new int[] {1});
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithTwoNumbersTest() {
+        highestProductOf3(new int[] {1, 1});
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
     }
 }
