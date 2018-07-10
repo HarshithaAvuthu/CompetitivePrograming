91f7dd88a206d3bb01e50260245409057f30f0b5
HarshithaAvuthu <31199062+HarshithaAvuthu@users.noreply.github.com>
2018-07-10 14:55:01 +0530

    Add files via upload

---
+++ Week2/day5/topscores.py
@@ -0,0 +1,58 @@
+import unittest
+
+def sort_scores(unsorted_scores, highest_possible_score):
+
+    # Sort the scores in O(n) time
+    
+    l1 = [0]*(highest_possible_score+1)
+    l2=[]*len(unsorted_scores)
+    for i in unsorted_scores:
+        l1[i]+=1
+    for i in range(highest_possible_score,-1,-1):
+        for j in range(l1[i]):
+            l2.append(i)
+    print l2   
+    return l2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_no_scores(self):
+        actual = sort_scores([], 100)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_one_score(self):
+        actual = sort_scores([55], 100)
+        expected = [55]
+        self.assertEqual(actual, expected)
+
+    def test_two_scores(self):
+        actual = sort_scores([30, 60], 100)
+        expected = [60, 30]
+        self.assertEqual(actual, expected)
+
+    def test_many_scores(self):
+        actual = sort_scores([37, 89, 41, 65, 91, 53], 100)
+        expected = [91, 89, 65, 53, 41, 37]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
